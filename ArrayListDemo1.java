ce8101a044a4e6219e38c2d38a7b40718ba7e52d
DhivyaBharkavi <52399831+DhivyaBharkavi@users.noreply.github.com>
2019-07-30 17:05:48 +0530

    ArrayList using sort method
    Then it change main method function to another class(alternate)

---
+++ ArrayListDemo1.java
@@ -0,0 +1,13 @@
+package exercise30july19;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListDemo1 {
+	public static void processing(ArrayList list)
+	{
+		System.out.println(list);
+		Collections.sort(list);
+		System.out.println(list);
+	}
+}
---
+++ ArrayListDemoTest1.java
@@ -0,0 +1,20 @@
+package exercise30july19;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListDemoTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList numbers = new ArrayList();
+		//ArrayListDemo1 numbers = new ArrayListDemo1();
+		numbers.add(5);
+		numbers.add(50);
+		numbers.add(15);
+		numbers.add(45);
+		numbers.add(53);
+		ArrayListDemo1.processing(numbers);
+	}
+
+}
