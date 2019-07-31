a4ad515c14bb6f4042abba3b0fe10008f0b3b318
DhivyaBharkavi <52399831+DhivyaBharkavi@users.noreply.github.com>
2019-07-31 12:43:57 +0530

    Exercise 31 July 19 Parameter

---
+++ DemoStudentInfo1.java
@@ -0,0 +1,11 @@
+package exercise31july19;
+
+import java.util.ArrayList;
+
+public class DemoStudentInfo1 {
+	//Parameter passing 
+	public void Studentlist(ArrayList al)
+	{
+		System.out.println(al);
+	}
+}
---
+++ DemoStudentInfoTest1.java
@@ -0,0 +1,20 @@
+package exercise31july19;
+
+import java.util.ArrayList;
+
+public class DemoStudentInfoTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList list = new ArrayList();
+		DemoStudentInfo1 s = new DemoStudentInfo1();
+		 list.add("Dhivya");
+		 list.add("Bharkavi");
+		 list.add("Anju");
+		 list.add("Goodwin");
+		 list.add("Bhuvaneswari");
+		 s.Studentlist(list);
+		 //System.out.println(s);
+	}
+
+}
